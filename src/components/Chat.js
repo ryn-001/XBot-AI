90eb01f6e779b5d1460e9573e99c592dfbf3ff93
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-19 22:23:58 +0530

    completed UI

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -53,15 +53,6 @@ export default function Chat() {
         }, 500);
     };
 
-    const handleFeedback = (idx, val) => {
-        const updated = currentChat.map((msg, i) => i === idx ? {
-            ...msg,
-            feedback: val,
-        } : msg);
-
-        setCurrentChat(updated);
-    }
-
 
     return (
         <div className="chat-section">
@@ -69,95 +60,42 @@ export default function Chat() {
                 <h3>Bot AI</h3>
             </div>
 
-            {showWelcome ? (<>
-                <div className="chat-heading">
-                    <h1>How can I help you today ?</h1>
-
-                    <div className="chat-pfp">
-                        <img src={pfp} alt="chat-pfp" />
-                    </div>
-                </div>
-
-                <div className="questions-section">
-                    <button className="question" onClick={() => handleSendMessage("Hi, what is the weather")}>
-                        <h3>Hi, what is the weather</h3>
-                        <p>Get immediate AI generated response</p>
-                    </button>
-
-                    <button className="question" onClick={() => handleSendMessage("Hi, what is my location")}>
-                        <h3>Hi, what is my location</h3>
-                        <p>Get immediate AI generated response</p>
-                    </button>
-
-                    <button className="question question-3" onClick={() => handleSendMessage("Hi, what is the temreature")}>
-                        <h3>Hi, what is the tempreature</h3>
-                        <p>Get immediate AI generated response</p>
-                    </button>
-
-                    <button className="question" onClick={() => handleSendMessage("Hi, how are you")}>
-                        <h3>Hi, how are you</h3>
-                        <p>Get immediate AI generated response</p>
-                    </button>
-                </div>
-
-            </>) : (<div className="chat-container">
-                {currentChat.length > 0 ? (
-                    currentChat.map((chat, idx) => (
-                        <div className="chat-messages">
-                            <div key={idx} className="chat-box">
-                                <div className="chat-pfp">
-                                    {chat.sender === "user" ? (<img className="chat-user-pfp" src={user} alt="user"></img>) : (<img className="chat-bot-pfp" src={pfp} alt="bot"></img>)}
+            {showWelcome ? (<div className="welcome-heading">
+                                <div className="welcome">
+                                    <h1>How can I Help You Today ?</h1>
+                                    <img src={pfp} alt="pfp"/>
                                 </div>
 
-                                <div className="chat-wrapper">
-                                    <div className="chat-msg">{chat.text}</div>
-                                    <div className="chat-time">{chat.time}</div>
+                                <div className="welcome-questions">
+                                    <button className="question" onClick={() => handleSendMessage("Hi, what is the weather")}>
+                                        <h3>Hi, what is the weather</h3>
+                                        <p>Get immediate AI generated response</p>
+                                    </button>
 
-                                    {chat.sender === "bot" && (
-                                        <>
-                                            <input
-                                                id="like"
-                                                type="radio"
-                                                name={`feedback-${idx}`}
-                                                value="like"
-                                                checked={chat.feedback === "like"}
-                                                onChange={() => handleFeedback(idx, "like")}
-                                            />{" "}
-                                            <label for="like" id="like-button">👍</label>
-                                            {"  "}
-                                            <label for="dislike" id="dislike-button">
-                                                👎
-                                            </label>
-                                            <input
-                                                id="dislike"
-                                                type="radio"
-                                                name={`feedback-${idx}`}
-                                                value="dislike"
-                                                checked={chat.feedback === "dislike"}
-                                                onChange={() => handleFeedback(idx, "dislike")}
-                                            />{" "}
-                                        </>
-                                    )}
+                                    <button className="question" onClick={() => handleSendMessage("Hi, what is my location")}>
+                                        <h3>Hi, what is my location</h3>
+                                        <p>Get immediate AI generated response</p>
+                                    </button>
+
+                                    <button className="question question-3" onClick={() => handleSendMessage("Hi, what is tempreature")}>
+                                        <h3>Hi, what is tempreature</h3>
+                                        <p>Get immediate AI generated response</p>
+                                    </button>
+
+                                    <button className="question" onClick={() => handleSendMessage("Hi, how are you")}>
+                                        <h3>Hi, how are you</h3>
+                                        <p>Get immediate AI generated response</p>
+                                    </button>
                                 </div>
-                            </div>
-                        </div>
-                    ))
-                ) : (<></>)}
-            </div>)}
+            </div>) : (<div className="current-chats">
+                        
+            </div>)}         
 
             <div className="input-section">
-                <input
-                    type="text"
-                    className="text-input"
-                    value={message}
-                    onChange={(e) => setCurrentMessage(e.target.value)}
-                />
-
-                <button className="send-button" onClick={() => handleSendMessage(message)}>Send</button>
-
-                <button className="save-button">Save</button>
-
-            </div>
+                <input type="text"/>    
+                <button id="send">Send</button>
+                <button id="save">Save</button>
+            </div>   
         </div>
     )
 }
